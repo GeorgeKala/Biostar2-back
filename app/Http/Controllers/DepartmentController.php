32ef9216c6861a7939e1157af0f56ec691c30e41
Giorgi Kalandadze <g.kalandadze@gorgia.ge>
2024-07-04 18:04:07 +0400

    feat/make departments table and controller for it

---
+++ app/Http/Controllers/DepartmentController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Department;
+
+class DepartmentController extends Controller
+{
+    public function index()
+    {
+        $departments = Department::all();
+        return response()->json(['departments' => $departments], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'parent_id' => 'nullable|exists:departments,id'
+        ]);
+
+        $department = Department::create($request->all());
+
+        return response()->json(['department' => $department], 201);
+    }
+
+    public function show(Department $department)
+    {
+        return response()->json(['department' => $department], 200);
+    }
+
+    public function update(Request $request, Department $department)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'parent_id' => 'nullable|exists:departments,id'
+        ]);
+
+        $department->update($request->all());
+
+        return response()->json(['department' => $department], 200);
+    }
+
+    public function destroy(Department $department)
+    {
+        $department->delete();
+
+        return response()->json(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BuildingController;
+use App\Http\Controllers\DepartmentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserTypeController;
@@ -31,3 +32,4 @@ Route::middleware('auth:sanctum')->group(function () {
 
 Route::apiResource('user-types', UserTypeController::class);
 Route::apiResource('buildings', BuildingController::class);
+Route::apiResource('departments', DepartmentController::class);
